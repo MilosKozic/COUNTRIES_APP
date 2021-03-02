f5f1289aa5d3bfd3a922d631d1f34053a1bd2ae0
MilosKozic <milos.kozic1986@gmail.com>
2021-03-02 14:09:02 +0100

    exterlanserver

--- src/components/service.js
+++ src/components/service.js
@@ -3,11 +3,11 @@ import axios from 'axios'
 export const getCountries =()=> axios.get('https://restcountries.eu/rest/v2/all')
 
 export const registerUser = (user) => {
-    return axios.post('http://localhost:3005/users',user)
+    return axios.post('https://my-json-server.typicode.com/MilosKozic/users-api/users',user)
 }
 
 export const getUsers = () => {
-    return axios.get('http://localhost:3005/users')
+    return axios.get('https://my-json-server.typicode.com/MilosKozic/users-api/users')
 }
 
 export const getCityForecast =(city)=> axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)

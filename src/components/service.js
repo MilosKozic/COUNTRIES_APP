63caa1edcc3de19704de313b2cb7b8b9e235a359
MilosKozic <milos.kozic1986@gmail.com>
2021-04-06 00:30:19 +0200

    register done

--- src/components/service.js
+++ src/components/service.js
@@ -3,11 +3,13 @@ import axios from 'axios'
 export const getCountries =()=> axios.get('https://restcountries.eu/rest/v2/all')
 
 export const registerUser = (user) => {
-    return axios.post('https://my-json-server.typicode.com/MilosKozic/users-api/users',user)
+    // return axios.post('https://my-json-server.typicode.com/MilosKozic/users-api/users',user)
+    return axios.post('https://nodejs-mongodb-regg-app.herokuapp.com/users',user)
 }
 
 export const getUsers = () => {
-    return axios.get('https://my-json-server.typicode.com/MilosKozic/users-api/users')
+    // return axios.get('https://my-json-server.typicode.com/MilosKozic/users-api/users')
+    return axios.get('https://nodejs-mongodb-regg-app.herokuapp.com/users')
 }
 
 export const getCityForecast =(city)=> axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
